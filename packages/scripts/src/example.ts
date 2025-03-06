16e6c38f909873d093f02d633dcfc8b2ac3778ea
Bob Gibilaro <bobgibilaro@Bobs-MacBook-Pro.local>
2025-03-06 13:33:42 -0600

    Initial commit

--- packages/scripts/src/example.ts
+++ packages/scripts/src/example.ts
@@ -1,4 +1,4 @@
 import { Resource } from "sst";
-import { Example } from "@monorepo-template/core/example";
+import { Example } from "@gibcal-sst-mono-1/core/example";
 
 console.log(`${Example.hello()} Linked to ${Resource.MyBucket.name}.`);
